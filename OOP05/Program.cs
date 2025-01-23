ded251fc06bf19f7c583fcff32bf4f2e8df15d89
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-24 00:33:54 +0200

    Static [Class, Atrribute, Property, Constructor, Method] and Const

--- OOP05/Program.cs
+++ OOP05/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             #region Abstraction [Abstract Class, Abstract Method, Abstract Property]
-           
+
             /// Shape shape;
             /// 
             ///  //Shape shape = new Shape();
@@ -33,6 +33,21 @@
             ///
             ///Console.WriteLine($"Circle Peri = {circleperi}");
             ///Console.WriteLine($"Circle Area = : {circlleArea}"); 
+
+            #endregion
+
+            #region Static [Class, Atrribute, Property, Constructor, Method] and Const
+           
+            //Utility U01 = new Utility(1, 2);
+            //Utility U02 = new Utility(5, 20);
+
+            // The result of calling this method will not be different by the difference of the Object
+
+            /// Console.WriteLine(Utility.CmToInch(23));
+            /// Console.WriteLine(Utility.CmToInch(251));
+         
+            // Console.WriteLine($"Circle Area = {Utility.CalcCircleArea(10)}");
+            // Console.WriteLine($"Circle Area = {Utility.CalcCircleArea(10)}");
             
             #endregion
         }
