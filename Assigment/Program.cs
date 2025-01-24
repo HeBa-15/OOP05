139bba71b54a007b27cf8f7281850ed68d9ed2a9
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-24 14:46:02 +0200

    Second Project

--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -20,13 +20,13 @@
             Z = z;
         }
 
-        // 2. Override the ToString Function
+
         public override string ToString()
         {
             return $"Point Coordinates: ({X}, {Y}, {Z})";
         }
 
-        // 4. Override Equals and GetHashCode for ==
+
         public override bool Equals(object obj)
         {
             if (obj is Point3D other)
@@ -51,13 +51,13 @@
             return !Equals(left, right);
         }
 
-        // 6. Implement ICloneable
+
         public object Clone()
         {
             return new Point3D(X, Y, Z);
         }
 
-        // Implement IComparable
+
         public int CompareTo(Point3D other)
         {
             if (X != other.X)
@@ -66,8 +66,40 @@
             }
             return Y.CompareTo(other.Y);
         }
-    } 
+    }
     #endregion
+
+
+
+
+    public class Maths
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static double Divide(int x ,int y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return (double)x / y;
+        }
+    }
+
+
 
 
     internal class Program
@@ -75,59 +107,78 @@
         public static void Main()
         {
 
-            Point3D P1 = ReadPoint("P1");
-            Point3D P2 = ReadPoint("P2");
+            #region First Project
+        //    Point3D P1 = ReadPoint("P1");
+        //    Point3D P2 = ReadPoint("P2");
 
-            Console.WriteLine(P1.ToString());
-            Console.WriteLine(P2.ToString());
+        //    Console.WriteLine(P1.ToString());
+        //    Console.WriteLine(P2.ToString());
 
-            // 4. Try to use ==
-            if (P1 == P2)
-            {
-                Console.WriteLine("P1 and P2 are equal.");
-            }
-            else
-            {
-                Console.WriteLine("P1 and P2 are not equal.");
-            }
 
-            // 5. Define an array of points and sort this array based on X & Y coordinates
-            Point3D[] points = { P1, P2, new Point3D(1, 2, 3), new Point3D(4, 5, 6) };
-            Array.Sort(points);
+        //    if (P1 == P2)
+        //    {
+        //        Console.WriteLine("P1 and P2 are equal.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("P1 and P2 are not equal.");
+        //    }
 
-            Console.WriteLine("Sorted Points:");
-            foreach (var point in points)
-            {
-                Console.WriteLine(point.ToString());
-            }
-        }
 
-        private static Point3D ReadPoint(string pointName)
-        {
-            int x, y, z;
+        //    Point3D[] points = { P1, P2, new Point3D(1, 2, 3), new Point3D(4, 5, 6) };
+        //    Array.Sort(points);
 
-            Console.WriteLine($"Enter coordinates for {pointName}:");
+        //    Console.WriteLine("Sorted Points:");
+        //    foreach (var point in points)
+        //    {
+        //        Console.WriteLine(point.ToString());
+        //    }
+        //}
 
-            Console.Write("X: ");
-            while (!int.TryParse(Console.ReadLine(), out x))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer for X.");
-            }
+        //private static Point3D ReadPoint(string pointName)
+        //{
+        //    int x, y, z;
 
-            Console.Write("Y: ");
-            while (!int.TryParse(Console.ReadLine(), out y))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer for Y.");
-            }
+        //    Console.WriteLine($"Enter coordinates for {pointName}:");
 
-            Console.Write("Z: ");
-            while (!int.TryParse(Console.ReadLine(), out z))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer for Z.");
-            }
+        //    Console.Write("X: ");
+        //    while (!int.TryParse(Console.ReadLine(), out x))
+        //    {
+        //        Console.WriteLine("Invalid input. Please enter an integer for X.");
+        //    }
 
-            return new Point3D(x, y, z);
+        //    Console.Write("Y: ");
+        //    while (!int.TryParse(Console.ReadLine(), out y))
+        //    {
+        //        Console.WriteLine("Invalid input. Please enter an integer for Y.");
+        //    }
+
+        //    Console.Write("Z: ");
+        //    while (!int.TryParse(Console.ReadLine(), out z))
+        //    {
+        //        Console.WriteLine("Invalid input. Please enter an integer for Z.");
+        //    }
+
+        //    return new Point3D(x, y, z);
+
+            #endregion
+
+        
+
+            int x = 10;
+            int y = 2;
+
+            Console.WriteLine($"Add: {Maths.Add(x, y)}");
+            Console.WriteLine($"Subtract: {Maths.Subtract(x, y)}");
+            Console.WriteLine($"Multiply: {Maths.Multiply(x, y)}");
+            Console.WriteLine($"Divide: {Maths.Divide(x, y)}");
+
         }
     }
 
+
+
+
+
 }
+
